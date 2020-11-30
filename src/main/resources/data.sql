43fff68ddad63aa06258cfa69adcd317b34fe5c5
Pawel <pawel.volt@gmail.com>
2020-11-30 21:22:46 +0100

    New sql test data added

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,8 @@
 INSERT INTO carStorage (model, year, milage, price) values("Tesla s", 2020, 100, 200000);
 INSERT INTO carStorage (model, year, milage, price) values("BMW", 2000, 300000, 10000);
 INSERT INTO carStorage (model, year, milage, price) values("Dacia", 2015, 50000, 10000);
+INSERT INTO carStorage (model, year, milage, price) values("Kia", 2020, 10, 75000);
 INSERT INTO carStorage (model, year, milage, price) values("Opel", 2010, 3000, 9500);
+INSERT INTO carStorage (model, year, milage, price) values("Hyundai", 2019, 3000, 70000);
+INSERT INTO carStorage (model, year, milage, price) values("Skoda", 2018, 15000, 55000);
+INSERT INTO carStorage (model, year, milage, price) values("Renault", 2000, 150000, 12000);
