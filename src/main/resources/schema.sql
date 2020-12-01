16fa52727575c6db27e3143a7a3b04eec0e845c6
Pawel <pawel.volt@gmail.com>
2020-12-01 19:43:48 +0100

    Improvements to sql: created additional views.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,3 +6,6 @@ milage integer NOT NULL,
 price integer NOT NULL,
 CONSTRAINT pk_car_car_id PRIMARY KEY (car_id)
 );
+
+CREATE VIEW PRICE_SORT_ASC AS SELECT * FROM carStorage ORDER BY PRICE ASC;
+CREATE VIEW BELOW_10_YEARS AS SELECT * FROM carStorage WHERE year >= YEAR(CURDATE()) - 10 ORDER BY YEAR desc;
